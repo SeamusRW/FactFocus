55056fda23c25f793a9385c12e0762a38c9ee14a
John Walker <seamusw934@gmail.com>
2023-05-05 15:10:26 -0400

    Add project files.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FactFocus
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
